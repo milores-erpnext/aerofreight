8d833d4369852406c45ec71dd8b34c3003c4b18f
abhishek-milores-tech <abhishek.prakash@milores.net>
2026-09-15 09:29:12 +0000

    Added changes

--- aerofreight/aerofreight/doctype/credit_agreement/credit_agreement.py
+++ aerofreight/aerofreight/doctype/credit_agreement/credit_agreement.py
@@ -1,9 +1,41 @@
 # Copyright (c) 2026, Milores and contributors
 # For license information, please see license.txt
 
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 
 class CreditAgreement(Document):
-	pass
+    def validate(self):
+        self.update_customer_credit_limit()
+
+    def update_customer_credit_limit(self):
+        if not self.customer_name or not self.company:
+            return
+
+        customer = frappe.get_doc("Customer", self.customer_name)
+
+        credit_limit = frappe.utils.flt(
+            self.equested_credit_equested_credit__limit_optional
+        )
+        credit_days = frappe.utils.cint(self.requested_credit_days)
+
+        # Check if company already exists in Customer Credit Limits
+        existing_row = None
+        for row in customer.credit_limits:
+            if row.company == self.company:
+                existing_row = row
+                break
+
+        if existing_row:
+            existing_row.credit_limit = credit_limit
+            existing_row.credit_days = credit_days
+        else:
+            customer.append("credit_limits", {
+                "company": self.company,
+                "credit_limit": credit_limit,
+                "custom_customer_credit_days_": credit_days,
+                "bypass_credit_limit_check": 0
+            })
+
+        customer.save(ignore_permissions=True)
